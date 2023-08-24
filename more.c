213bfae66b243467c00fa67c650da0cb5ec00136
Pibo Shongwe <piboman@yahoo.com>
2023-08-24 21:59:26 +0200

    Update more.c

--- more.c
+++ more.c
@@ -36,3 +36,47 @@ if (sign)
 *--ptr = sign;
 return (ptr);
 }
+
+/**
+ * put_pointer - Prints the value of a pointer in hexadecimal format.
+ * @args: A va_list containing the pointer to be printed.
+ *
+ * Return: The number of characters printed.
+ */
+int put_pointer(va_list args)
+{
+void *ptr = va_arg(args, void *);
+char buffer[20];
+int charCount;
+unsigned long long int num;
+int i;
+
+charCount = 0;
+charCount += _putchar('0');
+charCount += _putchar('x');
+num = (unsigned long long int)ptr;
+
+
+i = 0;
+if (num == 0)
+return (_putchar('0'));
+
+while (num > 0)
+{
+int remainder = num % 16;
+if (remainder < 10)
+{
+buffer[i++] = remainder + '0';
+}
+else 
+{
+buffer[i++] = remainder - 10 + 'a';
+}
+num /= 16;
+}
+
+for (int j = i - 1; j >= 0; j--)
+charCount += _putchar(buffer[j]);
+
+return (charCount);
+}
