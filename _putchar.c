c22e00192d5890474d3dc8b138359f6e9c396a4e
Pibo Shongwe <piboman@yahoo.com>
2023-08-24 21:35:01 +0200

    Update _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -2,23 +2,23 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
 
 /**
- * _puts - Prints a string to the standard output.
- * @str: The string to be printed.
- *
- * Return: The number of characters printed.
- */
+* _puts - Prints a string to the standard output.
+* @str: The string to be printed.
+*
+* Return: The number of characters printed.
+*/
 int _puts(const char *str)
 {
 int i, charCount;
@@ -30,19 +30,20 @@ return (charCount);
 }
 
 /**
- * _put_unsigned - Prints an unsigned integer to the standard output.
- * @num: The unsigned integer to be printed.
- *
- * Return: The number of characters printed.
- */
+* _put_unsigned - Prints an unsigned integer to the standard output.
+* @num: The unsigned integer to be printed.
+*
+* Return: The number of characters printed.
+*/
 int _put_unsigned(va_list args)
 {
-int i, j, charCount, num;
+int charCount, num;
 char *buffer;
 
+num = va_arg(args, unsigned int);
 if (num == 0)
 return (_putchar('0'));
-buffer = itoa(va_arg(args, unsigned int), 10);
+buffer = itoa(num, 10);
 charCount = _puts((buffer != NULL) ? buffer : "NULL");
 return (charCount);
 }
@@ -70,17 +71,17 @@ return (charCount);
 }
 
 /**
- * put_unsigned_hex - Prints the hexadecimal representation of an unsigned integer.
- * @args: A va_list containing the unsigned integer to be printed in hexadecimal.
- *
- * Return: The number of characters printed.
- */
+* put_unsigned_hex - Prints the hexadecimal representation of an unsigned integer.
+* @args: A va_list containing the unsigned integer to be printed in hexadecimal.
+*
+* Return: The number of characters printed.
+*/
 int put_unsigned_hex(va_list args)
 {
 unsigned int num, i, j, remainder;
 char buffer[20];
 int charCount;
-	
+
 num = va_arg(args, unsigned int);
 if (num == 0)
 return _putchar('0');
