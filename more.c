3a394540c64fd1127b52b5154e82eafa76d707dc
Pibo Shongwe <piboman@yahoo.com>
2023-08-24 21:31:45 +0200

    Create more.c

---
+++ more.c
@@ -0,0 +1,35 @@
+/**
+ * itoa - int to ascii
+ * @num: num
+ * @base: base
+ *
+ * Return: char
+ **/
+char *itoa(long int num, int baseNum)
+{
+static char *array = "0123456789abcdef";
+static char buffer[50];
+char *ptr;
+char sign;
+unsigned long n;
+
+n = num;
+sign = 0;
+if (num < 0)
+{
+n = -num;
+sign = '-';
+}
+ptr = &buffer[49];
+*ptr = '\0';
+
+while (n != 0)
+{
+*--ptr = array[n % baseNum];
+n /= baseNum;
+}
+
+if (sign)
+*--ptr = sign;
+return (ptr);
+}
